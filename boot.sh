0f0f918e62a4a8b9e32fd183482d28da74da19b2
Alex Parra <alexpds@gmail.com>
2020-05-21 23:45:10 +0100

    Init tests

--- boot.sh
+++ boot.sh
@@ -1,5 +1,6 @@
 
 case $1 in
   dev) FLASK_ENV=development flask run --reload ;;
+  test) python -m unittest -v test_app.py ;;
   *) echo "Unknow command"
 esac
--- test_app.py
+++ test_app.py
@@ -1,1 +1,22 @@
+import os
+import unittest
+from app import create_app
 
+
+class CapstoneTestCase(unittest.TestCase):
+    """This class represents the app test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+
+    def tearDown(self):
+        """Executed after each test"""
+        pass
+
+    def test_get_index(self):
+        """Test get_index """
+        res = self.client().get("/")
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json["msg"], "Hello")
