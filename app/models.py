276f8b5ffa0bd84dae17193be178c0e69d505481
Alex Parra <alexpds@gmail.com>
2020-05-23 00:15:27 +0100

    Actor model

--- app/models.py
+++ app/models.py
@@ -26,6 +26,7 @@ class Movie(db.Model):
         - title: string
         - release_date: string
         Methods:
+        - data
         - insert
         - update
         - delete
@@ -41,11 +42,57 @@ class Movie(db.Model):
         self.title = title
         self.release_date = release_date
 
-    def format(self):
+    def data(self):
         return {
             "id": self.id,
             "title": self.title,
             "release_date": self.release_date,
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Actor(db.Model):
+    """
+    Actor Model
+        Attributes:
+        - name: string
+        - age: int
+        - gender: M | F | X
+        Methods:
+        - data
+        - insert
+        - update
+        - delete
+    """
+
+    __tablename__ = "actors"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def data(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
         }
 
     def insert(self):
