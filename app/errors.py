c28d548f34af1ca4f38ddc4b8ab484cddbfaeed5
Alex Parra <alexpds@gmail.com>
2020-05-23 00:15:15 +0100

    Update errors

--- app/errors.py
+++ app/errors.py
@@ -9,31 +9,37 @@ def setup_errors(app):
     # Handle 400 Bad Request
     @app.errorhandler(400)
     def error400(error):
-        msg = "bad request"
+        msg = "Bad Request"
         return jsonify({"success": False, "error": 400, "message": msg}), 400
 
     # Handle 401 Unauthorized
     @app.errorhandler(401)
     def error401(error):
-        msg = "unauthorized"
+        msg = "Unauthorized"
         return jsonify({"success": False, "error": 401, "message": msg}), 401
 
     # Handle 403 Forbidden
     @app.errorhandler(403)
     def error403(error):
-        msg = "forbidden"
+        msg = "Forbidden"
         return jsonify({"success": False, "error": 403, "message": msg}), 403
 
     # Handle 404 Not Found
     @app.errorhandler(404)
     def error404(error):
-        msg = "not found"
+        msg = "Not Found"
         return jsonify({"success": False, "error": 404, "message": msg}), 404
 
-    # Handle 422 unprocessable entity
+    # Handle 405 Not Allowed
+    @app.errorhandler(405)
+    def error405(error):
+        msg = "Not Allowed"
+        return jsonify({"success": False, "error": 405, "message": msg}), 405
+
+    # Handle 422 Unprocessable Entity
     @app.errorhandler(422)
     def error422(error):
-        msg = "unprocessable"
+        msg = "Unprocessable"
         return jsonify({"success": False, "error": 422, "message": msg}), 422
 
     # Handle 500 server error
